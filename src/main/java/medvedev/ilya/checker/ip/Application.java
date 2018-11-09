5a41a093f06d78a51339a600a97cbb5ed450557f
Ilya Medvedev <medved55rus@gmail.com>
2018-11-09 11:01:27 +0000

    application context closing fix

--- src/main/java/medvedev/ilya/checker/ip/Application.java
+++ src/main/java/medvedev/ilya/checker/ip/Application.java
@@ -3,13 +3,12 @@ package medvedev.ilya.checker.ip;
 import medvedev.ilya.checker.ip.service.checker.CheckerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
     public static void main(final String[] args) {
-        try (ConfigurableApplicationContext context = SpringApplication.run(Application.class, args)) {
-            context.getBean(CheckerService.class).run();
-        }
+        SpringApplication.run(Application.class, args)
+                .getBean(CheckerService.class)
+                .run();
     }
 }
